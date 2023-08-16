d11525d193a359133dfaa775da0272946322c3f7
CSatWMS <christinasnyder@westminster.net>
2023-08-16 13:56:45 -0400

    Intro Day 2

---
+++ APCSA_Sixth/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(1992);
+
+        System.out.println("M\\s. Snyder");
+        System.out.print("Westminster");
+        System.out.print("Schools");
+        System.out.println("hooray");
+        System.out.println("awesome");
+    }
+}
