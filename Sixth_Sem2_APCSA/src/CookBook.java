605243a9c1169453558f32d408960bab258d5cbf
CSatWMS <christinasnyder@westminster.net>
2024-02-01 08:50:47 -0500

    More Recipe Stuff

--- Sixth_Sem2_APCSA/src/CookBook.java
+++ Sixth_Sem2_APCSA/src/CookBook.java
@@ -1,21 +1,18 @@
 public class CookBook {
     public static void main(String[] args) {
+        //Make an ingredient
+        //print it out
         Ingredient i1 = new Ingredient(1, "tsp", "salt");
         System.out.println(i1);
 
         Recipe r1 = new Recipe("Papi's Rice", 3, 25, 8);
-
         r1.addIngr(i1);
-        r1.addIngr(1, "cups", "rice");
+        r1.addIngr(1, "cup", "rice");
         r1.addIngr(2, "cups", "water");
-        r1.addIngr(1, "TBSP", "Oil");
-
-        r1.addStep("Sautee rice in oil");
-        r1.addStep("Add water");
-        r1.addStep("Bring to a boil");
-        r1.addStep("Heat Pan", 0);
+        r1.addIngr(1, "TBSP", "oil");
         System.out.println(r1);
 
-        Recipe doubleBatch = r1.scaleIt(2);
+        Recipe tripleBatch = r1.scaleIt(3);
+        System.out.println(tripleBatch);
     }
 }
---
+++ Sixth_Sem2_APCSA/src/Ingredient.java
@@ -0,0 +1,53 @@
+public class Ingredient {
+    //private dataType varName;
+    private double amount;
+    private String unit;
+    private String name;
+
+    /*
+    public ClassName(paramDT paramName, etc) {
+        this.varName = paramName;
+    }
+     */
+
+    public Ingredient(double amount, String u, String n){
+        this.amount = amount;
+        unit = u;
+        name = n;
+    }
+
+    public String toString(){
+        return amount + " " + unit + " " + name;
+    }
+
+    //public String toString() { return str; }
+
+    //public dt getVarName() { return varName; }
+
+    //public void setVarName(dt paramName) { this.varName = paramName; }
+
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
