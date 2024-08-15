75b2baaa4e15c6e6f3dc58abff9ac16aa917cba2
CSatWMS <christinasnyder@westminster.net>
2024-08-15 10:16:57 -0400

    First Period Day 1

---
+++ FirstPeriod/Unit1/src/Day1.java
@@ -0,0 +1,13 @@
+public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Ms. Snyder!");
+        System.out.println("I love pineapple pizza");
+        System.out.println("I love board games");
+        System.out.println(3 + 6);
+        System.out.println(3 - 7);
+        System.out.println(3 * 7);
+        System.out.println(6 / 7);
+        System.out.println(6 / 7.0);
+
+    }
+}
