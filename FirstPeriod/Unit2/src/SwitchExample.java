5fe3ce245022ca60f128625251f8adaa1c1aeba3
CSatWMS <christinasnyder@westminster.net>
2024-09-12 15:26:57 -0400

    Switch Example

---
+++ FirstPeriod/Unit2/src/SwitchExample.java
@@ -0,0 +1,19 @@
+public class SwitchExample {
+    public static void main(String[] args) {
+        String dayOfWeek = "R";
+        switch (dayOfWeek){
+            case "M" , "T" , "W":
+                System.out.println("Bored");
+                break;
+            case "R", "F":
+                System.out.println("Happy");
+                break;
+            case "Sa", "Su":
+                System.out.println("Excited!");
+                break;
+            default:
+                System.out.println("Invalid day!");
+                break;
+        }
+    }
+}
