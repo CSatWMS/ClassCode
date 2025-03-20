5264039894fbd2cb8350f2128e8b643ff1a0286a
CSatWMS <christinasnyder@westminster.net>
2025-03-20 08:44:48 -0400

    Abstract ComicCharacter

---
+++ SixthPeriod/Unit8/src/ComicCharacter.java
@@ -0,0 +1,24 @@
+public abstract class ComicCharacter {
+    private String name, gender, occupation, universe;
+    private int age;
+
+    public ComicCharacter(String name, String gender, int age, String universe, String occupation){
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.universe = universe;
+        this.occupation = occupation;
+    }
+
+    public abstract void interactWith(ComicCharacter other);
+
+    public void says(String message){
+        System.out.println(name + " says " + message);
+    }
+
+    public String toString(){
+        String toReturn = name + " , from " + universe + " is " + age + " years old.";
+        toReturn += "\nThey work as a " + occupation + " and are a " + gender;
+        return toReturn;
+    }
+}
