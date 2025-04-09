7aa257400d6fa93bd5524194849c377f5bfe44dd
CSatWMS <christinasnyder@westminster.net>
2025-04-09 13:36:05 -0400

    Recursion level 2

--- FirstPeriod/Unit9/src/RecursionBasics.java
+++ FirstPeriod/Unit9/src/RecursionBasics.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class RecursionBasics {
     public static void main(String[] args) {
         //Pillars Of Recursion
@@ -8,8 +10,69 @@ public class RecursionBasics {
         System.out.println(factorial(200));
         //System.out.println(factorial(23)); Integer Overflow
         //System.out.println(factorial(-3)); Stack overflow error
-        System.out.println(fib(46));
+        //System.out.println(fib(46));
+
+        //   return a + multiply(a, b - 1);
+        System.out.println(multiply(3, 5));
+        System.out.println(multiply(-3, 5));
+        System.out.println(multiply(3, -5));
+        System.out.println(multiply(-3, -5));
+        System.out.println(isPalindrome("racecar"));
+        System.out.println(isPalindrome("racecat"));
+        System.out.println(isPalindrome("racekar"));
+        System.out.println(isPalindrome("Madam I'm Adam"));
+        System.out.println(getPermutations("eat"));
+        System.out.println(getPermutations("seat"));
     }
+
+    public static ArrayList<String> getPermutations(String str){
+        if (str.length() == 1){
+            ArrayList<String> toReturn = new ArrayList<String>();
+            toReturn.add(str);
+            return toReturn;
+        } else {
+            ArrayList<String> toReturn = new ArrayList<String>();
+            for (int i = 0; i < str.length(); i++){
+                char currentLetter = str.charAt(i);
+                String otherGuys = str.substring(0, i) + str.substring(i + 1);
+                ArrayList<String> miniPerms = getPermutations(otherGuys);
+                for (String guy : miniPerms){
+                    toReturn.add(currentLetter + guy);
+                }
+            }
+            return toReturn;
+        }
+    }
+
+    public static boolean isPalindrome(String str){
+        //all the same case (.toLowerCase() and .toUpperCase())
+        //want no spaces (str.charAt(i) != ' ')
+        //no punctuation (Character.isAlphabetic()
+            //char >= 'a' && char <= 'z'
+        str = str.toLowerCase();
+        String goodGuy = "";
+        //loop
+        for (int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if (c >= 'a' && c <= 'z'){
+                goodGuy += c;
+            }
+        }
+        return isPalindrome_rec(goodGuy);
+
+    }
+
+    public static boolean isPalindrome_rec(String str){
+        if (str.length() <= 1){
+            return true;
+        }
+        if (str.charAt(0) != str.charAt(str.length() - 1)){
+            return false;
+        }
+        return isPalindrome_rec(str.substring(1, str.length() - 1));
+
+    }
+
 
     public static int add(int a, int b){
         // NO! return a + b;
@@ -24,10 +87,22 @@ public class RecursionBasics {
     }
 
     public static int multiply(int a, int b){
+        if (a > 0 && b > 0){
+            return rec_multiply(a, b);
+        } else if (a < 0 && b < 0){
+            return rec_multiply(-a, -b);
+        } else if (b < 0){
+            return -rec_multiply(a, -b);
+        } else {
+            return rec_multiply(a, b);
+        }
+    }
+
+    public static int rec_multiply(int a, int b){
         if (a == 0 || b == 0){
             return 0;
         }
-        return a + multiply(a, b - 1);
+        return a + rec_multiply(a, b - 1);
         //return b + multiple(a - 1, b);
     }
 
